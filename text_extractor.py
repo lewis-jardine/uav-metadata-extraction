d099e90ff7ea012537f63d7c8fa1007d8329f7ef
Lewis Jardine <lewisjardine777@gmail.com>
2022-07-14 21:16:29 +0100

    Added contour detection to crop frames to text before analysis

--- text_extractor.py
+++ text_extractor.py
@@ -6,47 +6,57 @@ import pytesseract
 
 def text_detect(frame):
     # takes frames, finds text and appended to txt
+    # crop to only top and bottom of frame containing text, and convert to grayscale
+    crops = crop_frame(frame)
 
+    # store final processed frames
+    processed_frames = []
+    # apply all stages to each frame
+    for crop in crops:
+            
+        # apply adaptive threshold
+        thresh1 = cv2.adaptiveThreshold(crop, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 9)
+
+        # specify structure shape and kernel size
+        # kernel size increases or decreases size of rect to be detected
+        rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (40, 40))
+
+        # apply morphological transform
+        morph = cv2.morphologyEx(thresh1, cv2.MORPH_BLACKHAT, rect_kernel)
+        processed_frames.append(morph)
+
+        # append text to file
+        text = pytesseract.image_to_string(morph)
+        file = open("out_text.txt", "a")
+        file.write(text)
+        file.close()
+
+    return processed_frames
+
+
+def crop_frame(frame):
+    # crop frame to only show text
     # turn frame to gray scale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # perform OTSU threshold
-    ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+    # apply threshold
+    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, 35)
 
-    # specify structure shape and kernel size
-    # kernel size increases or decreases size of rect to be detected
-    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
+    # dilate to emphasise text blob
+    crop_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 8))
+    dilation = cv2.dilate(thresh, crop_kernel, iterations=2)
+    bounds, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    boxes = []
 
-    # apply dilation on threshold image
-    dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
-    
-    # find contours
-    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    
-    # copy input frame
-    processed_frame = frame.copy()
-
-    # loop through contours that could be text
-    # crop those rects and pass to pytesseract for extraction
-    # append extracted text to text file
-    for cnt in contours:
+    # loop through bounds, add them to boxes to be sorted by largest then returned
+    for cnt in bounds:
         x, y, w, h = cv2.boundingRect(cnt)
+        region = gray[y: y+ h, x:x + w]
+        boxes.append(region)
 
-        # draw rectangle on copied image
-        cv2.rectangle(processed_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    boxes = sorted(boxes, key=lambda i: -1 * i.shape[0] * i.shape[1])
 
-        # crop text block to give input for OCR
-        cropped = processed_frame[y:y + h, x:x +w]
-
-        # apply OCR to cropped areas
-        text = pytesseract.image_to_string(cropped)
-
-        # append recognised text to file
-        file = open("out_text.txt", "a")
-        file.write(text + "\n")
-        file.close()
-
-    return processed_frame
+    return boxes[:2]
 
 
 # construct arg parser
@@ -58,6 +68,9 @@ args = vars(ap.parse_args())
 # load the video file ref
 video = cv2.VideoCapture(args["video"])
 print("[INFO] video file loaded!")
+
+# create new file for text output, or overwrite if exists
+open("out_text.txt", "w").close()
 
 # get desired and actual fps
 desired_fps = args["fps"]
@@ -87,7 +100,7 @@ while True:
     orig_frame = frame.copy()
 
     # append end of frame marker to file
-    file = open("out_text.txt", "a+")
+    file = open("out_text.txt", "a")
     file.write("\n---frame:{}---\n".format(current_frame))
     file.close()
 
@@ -95,7 +108,8 @@ while True:
     processed_frame = text_detect(frame)
 
     # show input and output frames
-    cv2.imshow("Processed Video", processed_frame)
+    cv2.imshow("Processed Video 1", processed_frame[0])
+    cv2.imshow("Processed Video 2", processed_frame[1])
     
     # prints current frame no.
     print("frame:" + str(current_frame))
