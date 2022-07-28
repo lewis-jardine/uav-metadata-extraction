c67fdd13ee773af88a6ff0c678c8c6709c1bb6fc
Lewis Jardine <lewisjardine777@gmail.com>
2022-07-28 12:57:37 +0100

    feat: ROIs can now be manually selected

--- text_extractor.py
+++ text_extractor.py
@@ -1,62 +1,56 @@
 import argparse
-import argparse
 import numpy as np
 import cv2
 import pytesseract
 
 def text_detect(frame):
     # takes frames, finds text and appended to txt
-    # crop to only top and bottom of frame containing text, and convert to grayscale
-    crops = crop_frame(frame)
-
-    # store final processed frames
-    processed_frames = []
-    # apply all stages to each frame
-    for crop in crops:
-            
-        # apply adaptive threshold
-        thresh1 = cv2.adaptiveThreshold(crop, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 9)
-
-        # specify structure shape and kernel size
-        # kernel size increases or decreases size of rect to be detected
-        rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (40, 40))
-
-        # apply morphological transform
-        morph = cv2.morphologyEx(thresh1, cv2.MORPH_BLACKHAT, rect_kernel)
-        processed_frames.append(morph)
-
-        # append text to file
-        text = pytesseract.image_to_string(morph)
-        file = open("out_text.txt", "a")
-        file.write(text)
-        file.close()
-
-    return processed_frames
-
-
-def crop_frame(frame):
-    # crop frame to only show text
     # turn frame to gray scale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # apply threshold
-    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, 35)
+    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 35, -10)
 
-    # dilate to emphasise text blob
-    crop_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 8))
-    dilation = cv2.dilate(thresh, crop_kernel, iterations=2)
-    bounds, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    boxes = []
+    processed_frame = cv2.GaussianBlur(thresh,(3,3),0)
 
-    # loop through bounds, add them to boxes to be sorted by largest then returned
-    for cnt in bounds:
-        x, y, w, h = cv2.boundingRect(cnt)
-        region = gray[y: y+ h, x:x + w]
-        boxes.append(region)
+    # append text to file
+    text = pytesseract.image_to_string(processed_frame, config='--psm 7 -c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    file = open("out_text.txt", "a")
+    file.write(text)
+    file.close()
 
-    boxes = sorted(boxes, key=lambda i: -1 * i.shape[0] * i.shape[1])
+    return processed_frame
 
-    return boxes[:2]
+# draw_rectangle global_vars
+drawing = False
+ix, iy = -1, -1
+coords = []
+frame = ""
+clone = ""
+roi_regions = []
+
+def draw_rectangle(event, x, y, flags, param):
+    global ix, iy, drawing, frame, clone, coords, roi_regions
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix, iy = x, y
+        coords = [(x, y)]
+        roi_regions.append(coords)
+        clone = frame.copy()
+    
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            frame = clone.copy()
+            cv2.rectangle(frame, (ix, iy), (x, y), (0, 255, 0), 2)
+            a = x
+            b = y
+            if a != x | b != y:
+                cv2.rectangle(frame, (ix, iy), (x, y), (0, 0, 0), -1)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        coords.append((x, y))
+        frame = clone.copy()
+        cv2.rectangle(frame, (ix, iy), (x, y), (0, 255, 0), 1)
 
 
 # construct arg parser
@@ -79,7 +73,29 @@ video_fps = int(video.get(cv2.CAP_PROP_FPS))
 # frame number iterator
 current_frame = 0
 
-# loop over frames of the video, adjust to desired FPS
+# select ROI's
+frame = video.read()[1]
+frame = np.array(frame)
+cv2.namedWindow('ROIs', cv2.WINDOW_GUI_NORMAL)
+cv2.setMouseCallback('ROIs', draw_rectangle)
+cv2.setWindowProperty('ROIs', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+while True:
+    cv2.imshow('ROIs', frame)
+
+    # undo last rectangle
+    key = cv2.waitKey(1) & 0xFF
+    if key == 8:
+        del roi_regions[-1]
+        del coords[-1]
+        frame = clone.copy()
+
+    # break when finished/ enter pressed
+    if key == 13:
+        cv2.destroyAllWindows()
+        break
+
+# loop over cropped frames of the video, adjust to desired FPS
 while True:
     # iterate frame count
     current_frame += 1
@@ -96,20 +112,32 @@ while True:
         print("[INFO] video completed, exiting...")
         break
 
-    # store original frame
-    orig_frame = frame.copy()
-
     # append end of frame marker to file
     file = open("out_text.txt", "a")
     file.write("\n---frame:{}---\n".format(current_frame))
     file.close()
 
-    # detects text, returns processed frame
-    processed_frame = text_detect(frame)
+    # crop frame to ROIs only, loop and process
+    n = 0
+    for roi in roi_regions:
+        x1, y1 = roi[0]
+        x2, y2 = roi[1]
+        # make x1, y1 top left coords
+        if x1 > x2:
+            x1, x2 = x2, x1
+        if y1 > y2:
+            y1, y2 = y2, y1
+        crop = frame[y1:y2, x1:x2]
 
-    # show input and output frames
-    cv2.imshow("Processed Video 1", processed_frame[0])
-    cv2.imshow("Processed Video 2", processed_frame[1])
+        # detects text, returns processed frame
+        processed_frame = text_detect(crop)
+
+        # individual window name to allow display of multiple
+        n += 1
+        name = "ROI: {}".format(n)
+
+        # show input and output frames
+        cv2.imshow(name, processed_frame)
     
     # prints current frame no.
     print("frame:" + str(current_frame))
